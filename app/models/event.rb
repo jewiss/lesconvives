3af66d1e4d026b685218726194fdcfca1b98b8be
Antoine Desaché <antoine.desache@gmail.com>
2020-02-24 17:24:20 +0100

    added validation and dependant destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
-  has_many :selected_restaurants
-  has_many :participants
+  has_many :selected_restaurants, dependent: :destroy
+  has_many :participants, dependent: :destroy
+  validates :name, :date, presence: true
 end
