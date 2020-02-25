dee4474df06747c9a5c0e8e1c6dcb500a7cef2ca
Antoine Desaché <antoine.desache@gmail.com>
2020-02-25 17:43:43 +0100

    added link to routes in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show] do
-    resources :addresses, only: %i[new create edit update destroy]
+    resources :addresses, only: %i[index new create edit update destroy]
   end
 
   resources :events, only: %i[show new create] do
