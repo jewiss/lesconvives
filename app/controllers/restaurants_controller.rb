b35f502feedac3a63d3be1d8b88f653bc80b6e0c
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-25 18:32:24 +0100

    restaurant dans un rayon donné enregistre dans DB

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,41 @@
+require 'json'
+require 'open-uri'
+
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.all
+    lng = 2.379717
+    lat = 48.865433
+    @results_restaurants = parse_google_api(lat, lng)
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
+  private
+
+  def parse_google_api(lat, lng)
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=1000&type=restaurant&key=AIzaSyAHPB_XgGpd8N7bomLX-AxRKXatr1PSIF4"
+    restaurants_serialized = open(url).read
+    restaurants = JSON.parse(restaurants_serialized)["results"]
+    final_restaurants = restaurants.map do |restaurant|
+      Restaurant.find_or_create_by(google_api_id: restaurant["id"]) do |resto|
+        resto.name = restaurant["name"]
+        resto.price_level = restaurant["price_level"]
+        resto.rating = restaurant["rating"]
+        resto.address = restaurant["vicinity"]
+        resto.google_api_id = restaurant["id"]
+      end
+    end
+
+    return final_restaurants
+
+  end
+
+
 end
+
+
+# url pour recuperer photo
+# https://lh3.googleusercontent.com/p/AF1QipO8VERZZG1DP2DVaaqliwvomk8agqq0LNceljCb=s1600-w400
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_115405) do
+ActiveRecord::Schema.define(version: 2020_02_25_171253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_115405) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "google_api_id"
   end
 
   create_table "selected_restaurants", force: :cascade do |t|
