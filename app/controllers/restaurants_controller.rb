34913cde8bafccaa18885a807da9dd9ead714485
romain barbier <romainbarbier1212@hotmail.com>
2020-02-26 12:11:05 +0100

    displaying meeting point and time to travel

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,8 +6,8 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
-    geographic_center
-    directions_to_geographic_center
+    @geographic_center = geographic_center
+    @directions = directions_to_geographic_center_distance_matrix_api
   end
 
   def show
@@ -30,13 +30,31 @@ class RestaurantsController < ApplicationController
     geographic_center = Geocoder::Calculations.geographic_center(geo_positions)
   end
 
-  def directions_to_geographic_center
+  # def directions_to_geographic_center_directions_api
+  #   @participants = Participant.all
+  #   @participants.each do |participant|
+  #     url = "https://maps.googleapis.com/maps/api/directions/json?&origin=#{participant.address.latitude.to_f},#{participant.address.longitude.to_f}&destination=#{geographic_center[0]},#{geographic_center[1]}&mode=transit&key=AIzaSyAHPB_XgGpd8N7bomLX-AxRKXatr1PSIF4"
+  #     directions_serialized = open(url).read
+  #     directions = JSON.parse(directions_serialized)
+  #   end
+  # end
+
+  def directions_to_geographic_center_distance_matrix_api
     @participants = Participant.all
+    directions = {}
     @participants.each do |participant|
-      url = "https://maps.googleapis.com/maps/api/directions/json?&origin=#{participant.address.latitude.to_f},#{participant.address.longitude.to_f}&destination=#{geographic_center[0]},#{geographic_center[1]}&mode=transit&key=AIzaSyAHPB_XgGpd8N7bomLX-AxRKXatr1PSIF4"
+      url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{participant.address.latitude},#{participant.address.longitude}&destinations=#{geographic_center[0]},#{geographic_center[1]}&mode=transit&units=metric&key=AIzaSyAHPB_XgGpd8N7bomLX-AxRKXatr1PSIF4"
       directions_serialized = open(url).read
-      directions = JSON.parse(directions_serialized)
+      parsed_directions = JSON.parse(directions_serialized)
+      transit_duration = parsed_directions['rows'].first['elements'].first['duration']['text']
+      transit_distance = parsed_directions['rows'].first['elements'].first['distance']['text']
+      # raise
+      directions[participant] = {
+        transit_duration: transit_duration,
+        transit_distance: transit_distance
+      }
     end
+    return directions
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :selected_restaurants
   validates :name, presence: true
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :geocode, :reverse_geocode, if: :will_save_change_to_address?
 end
