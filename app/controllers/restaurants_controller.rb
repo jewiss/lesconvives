8e89a80599f085a9802b0e6c4762a425485a4851
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-26 15:13:57 +0100

    resolve issue pictures restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,8 @@ class RestaurantsController < ApplicationController
     lng = 2.379717
     lat = 48.865433
     @results_restaurants = parse_google_api(lat, lng)
-    @restaurants = Restaurant.all
-    @geographic_center = geo_center_to_address(geographic_center)
-    @directions = directions_to_geographic_center_distance_matrix_api
+    # @geographic_center = geo_center_to_address(geographic_center)
+    # @directions = directions_to_geographic_center_distance_matrix_api
   end
 
   def show
@@ -19,7 +18,7 @@ class RestaurantsController < ApplicationController
   private
 
  def parse_google_api(lat, lng)
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=1000&type=restaurant&key=#{ENV["GOOGLE_MAPS_TOKEN"]}"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=200&type=restaurant&key=#{ENV["GOOGLE_MAPS_TOKEN"]}"
     restaurants_serialized = open(url).read
     restaurants = JSON.parse(restaurants_serialized)["results"]
     final_restaurants = restaurants.map do |restaurant|
@@ -31,8 +30,11 @@ class RestaurantsController < ApplicationController
         r.google_api_id = restaurant["id"]
 
       end
-      res = Net::HTTP.get_response(URI("https://maps.googleapis.com/maps/api/place/photo?key=#{ENV["GOOGLE_MAPS_TOKEN"]}&photoreference=#{restaurant["photos"][0]["photo_reference"]}&maxwidth=400"))
-      resto.photo_url = res['location']
+      if restaurant["photos"] && !resto.photo.attached?
+        res = Net::HTTP.get_response(URI("https://maps.googleapis.com/maps/api/place/photo?key=#{ENV["GOOGLE_MAPS_TOKEN"]}&photoreference=#{restaurant["photos"][0]["photo_reference"]}&maxwidth=400"))
+        file = URI.open(res['location'])
+        resto.photo.attach(io: file, filename: "#{restaurant["id"]}.jpg", content_type: 'image/jpg')
+      end
       resto.save!
       resto
 
@@ -43,7 +45,7 @@ class RestaurantsController < ApplicationController
 
 
   def geographic_center
-    retrive_participants_geo_positions
+    # retrive_participants_geo_positions
     geo_positions = []
     @participants = Participant.all
     @participants.each do |participant|
@@ -52,19 +54,19 @@ class RestaurantsController < ApplicationController
       coordinates << participant.address.longitude.to_f
       geo_positions << coordinates
     end
-    find_geographic_center
+    # find_geographic_center
     geographic_center = Geocoder::Calculations.geographic_center(geo_positions)
   end
 
-  def geo_center_to_address(geo_center)
-    geo_center = geographic_center
-    results = Geocoder.search([geo_center[0], geo_center[1]])
-    if results
-      full_address = results.first.data['address']
-      shortened_address = "#{full_address['address29']}, #{full_address['road']}, #{full_address['postcode']}, #{full_address['city']}"
-    end
-    return shortened_address
-  end
+  # def geo_center_to_address(geo_center)
+  #   geo_center = geographic_center
+  #   results = Geocoder.search([geo_center[0], geo_center[1]])
+  #   if results
+  #     full_address = results.first.data['address']
+  #     shortened_address = "#{full_address['address29']}, #{full_address['road']}, #{full_address['postcode']}, #{full_address['city']}"
+  #   end
+  #   return shortened_address
+  # end
 
   # def directions_to_geographic_center_directions_api
   #   @participants = Participant.all
