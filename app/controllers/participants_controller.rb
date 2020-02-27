d45c394202945f75478e618864672cef7f21b2d6
Antoine Desaché <antoine.desache@gmail.com>
2020-02-27 15:18:05 +0100

    added create a participant

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,6 +1,6 @@
 class ParticipantsController < ApplicationController
   def new
-    @participants = Participant.new()
+    @participant = Participant.new()
     @event = Event.find(params[:event_id])
     if params[:query].present?
       sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
@@ -11,8 +11,14 @@ class ParticipantsController < ApplicationController
   end
 
   def create
+    @participant = Participant.new(event_id: params[:event_id], user_id: params[:user])
+    @participant.address = @participant.user.addresses.find_by(name: 'Work')
+    @participant.save
+    redirect_to new_event_participant_path
   end
 
   def destroy
   end
+
+
 end
