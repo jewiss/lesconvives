da855c0b43d4d52daa2802ff8cbda35a3fe00433
eniko guti <eniko.guti@gmail.com>
2020-02-28 11:09:16 +0100

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,17 +71,17 @@ thanh_work = Address.create!(name: 'Home', full_address: '11 rue brillat-savarin
 puts 'addresses created'
 
 # Events
-after_gaudelet = Event.create! (Date.parse('2020-01-24'), name: 'Afterwork Gaudelet')
-lunch_dance = Event.create! (Date.parse('2020-02-03'), name: 'Lunch Dance Studio')
-lewagon_karaoke = Event.create! (Date.parse('2020-02-13'), name: 'Karaoke Le Wagon') 
-dinner_family = Event.create!(Date.parse('2020-02-22'), name: 'Family Dinner')
-demo_day = Event.create!(Date.parse('2020-03-06'), name: 'Demo Day Gaudelet')
-tinder_date = Event.create!(Date.parse('2020-03-08'), name: 'Tinder Date')
+after_gaudelet = Event.create!(date: Date.new(2020,1,24), name: 'Afterwork Gaudelet')
+lunch_dance = Event.create!(date: Date.parse('2020-02-03'), name: 'Lunch Dance Studio')
+lewagon_karaoke = Event.create!(date: Date.parse('2020-02-13'), name: 'Karaoke Le Wagon') 
+dinner_family = Event.create!(date: Date.parse('2020-02-22'), name: 'Family Dinner')
+demo_day = Event.create!(date: Date.parse('2020-03-06'), name: 'Demo Day Gaudelet')
+tinder_date = Event.create!(date: Date.parse('2020-03-08'), name: 'Tinder Date')
 
 puts "dinner created"
 # Participants
-romain1 = Participant.create!(event_id: dinner1.id, user_id: romain.id, owner: true, address_id: romain_work.id)
-lucien1 = Participant.create!(event_id: dinner1.id, user_id: lucien.id, owner: false, address_id: lucien_home.id)
+romain1 = Participant.create!(event_id: dinner_family.id, user_id: romain.id, owner: true, address_id: romain_work.id)
+lucien1 = Participant.create!(event_id: dinner_family.id, user_id: lucien.id, owner: false, address_id: lucien_home.id)
 puts "participant created"
 
 puts 'Event "Diner des familles" created'
