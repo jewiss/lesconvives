ebfaae8fd5e2137a336ba4607b985ea1f511e900
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-28 15:26:12 +0100

    put avatar in navbar et remove avatar sur certaines pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def show_navbar_condition
+    params[:action] != "home" && params[:controller] != "pages"
+    params[:action] != "new" && params[:controller] != "devise/sessions"
+  end
 end
