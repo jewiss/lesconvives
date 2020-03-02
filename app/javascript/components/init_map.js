9cd558744b79c66def3a4e804f11fd920611a03f
romain barbier <romainbarbier1212@hotmail.com>
2020-03-02 10:50:59 +0100

    removed info on maps

--- app/javascript/components/init_map.js
+++ app/javascript/components/init_map.js
@@ -4,7 +4,7 @@ const initMap = () => {
 
   const mapElement = document.getElementById('map');
   if (mapElement) { // don't try to build a map if there's no div#map to inject in
-    const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+    const map = new GMaps({ el: '#map', lat: 0, lng: 0, disableDefaultUI: true });
     const markers = JSON.parse(mapElement.dataset.markers);
     map.addMarkers(markers);
     if (markers.length === 0) {
