e1e9416538ee8f19a0b03c05ae5e71f7ed8b5c95
Antoine Desaché <antoine.desache@gmail.com>
2020-03-03 15:37:13 +0100

    added when a user has no address he cannot create and event

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
-  has_many :participants
+  has_many :participants, dependent: :destroy
   geocoded_by :full_address
   after_validation :geocode, :reverse_geocode, if: :will_save_change_to_full_address?
   TYPE = ["Home", "Work", "Other"].freeze
