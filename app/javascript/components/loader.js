88ac44de36b7377d06b5212a6f88843465af9f89
eniko guti <eniko.guti@gmail.com>
2020-03-03 17:13:09 +0100

    width added

--- app/javascript/components/loader.js
+++ app/javascript/components/loader.js
@@ -1,5 +1,5 @@
 const addLoader = () => {
-  const loader = document.getElementById('loader')
+  const loader = document.getElementById('loader');
   loader.addEventListener('click', (event)=> {
     const gifLoader = document.querySelector(".loader-gif")
     gifLoader.classList.add("active")
