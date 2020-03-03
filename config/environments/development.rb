95199f774de8adb720c85c0f951203860c6b8860
romain barbier <romainbarbier1212@hotmail.com>
2020-03-03 12:01:23 +0100

    set up PWA + fixed padding on homepage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.hosts << '2d7bce28.ngrok.io'
+  config.hosts << '91234489.ngrok.io'
 
   config.cache_classes = false
 
