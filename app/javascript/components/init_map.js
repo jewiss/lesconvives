f176aed69ca8318cf4db582b42190cfb95673df1
romain barbier <romainbarbier1212@hotmail.com>
2020-03-05 17:52:00 +0100

    first attempt

--- app/javascript/components/init_map.js
+++ app/javascript/components/init_map.js
@@ -12,9 +12,37 @@ const initMap = () => {
     } else if (markers.length === 1) {
       map.setCenter(markers[0].lat, markers[0].lng);
       map.setZoom(14);
+    } else if (window.location.hash == "#zoom") {
+      map.setCenter(markers[0].lat, markers[0].lng);
+      debugger
+      map.setZoom(16)
+      console.log(map.map.zoom)
     } else {
       map.fitLatLngBounds(markers);
-    }
+    };
+
+    map.markers.forEach((marker) => {
+      if (marker.details) {
+        marker.addListener('click', function() {
+          const element = document.querySelector(".resto-cards");
+          element.style.display = "block";
+          console.log(marker.details);
+          const restaurantName = marker.details.name;
+          document.querySelector(".link-show-resto").href=`/restaurants/${marker.details.id}?event=${marker.details.event}`
+          document.querySelector(".resto-img").src = `http://res.cloudinary.com/dykaopgu0/image/upload/c_fill/${marker.details.photo_key}`
+          document.querySelector(".resto-name").innerText = `${restaurantName}`;
+          const ratingIcon = '<i class="fas fa-star"></i>'
+          const ratingIconMissing = '<i class="fas fa-star" id="level-missing"></i>'
+          const rating = (ratingIcon.repeat(marker.details.rating) + ratingIconMissing.repeat(5 - marker.details.rating));
+          document.querySelector(".resto-rating").innerHTML = `${marker.details.food_category}  ${rating}`;
+          document.querySelector(".resto-short-address").innerText = `${marker.details.short_address}`;
+          const sym = "€"
+          const pricing = sym.repeat(marker.details.price_level)
+          document.querySelector(".card-trip-pricing").innerText = pricing;
+        });
+      };
+    });
+
     const meetpoint = document.getElementById('meetpoint')
     if (meetpoint) {
       meetpoint.addEventListener('click', (event) => {
@@ -24,32 +52,9 @@ const initMap = () => {
         } else {
           map.setZoom(16)
         };
-
-      console.log(map.map.zoom)
       });
-    }
+    };
 
-    map.markers.forEach((marker) => {
-    if (marker.details) {
-      marker.addListener('click', function() {
-        const element = document.querySelector(".resto-cards");
-        element.style.display = "block";
-        console.log(marker.details);
-        const restaurantName = marker.details.name;
-        document.querySelector(".link-show-resto").href=`/restaurants/${marker.details.id}?event=${marker.details.event}`
-        document.querySelector(".resto-img").src = `http://res.cloudinary.com/dykaopgu0/image/upload/c_fill/${marker.details.photo_key}`
-        document.querySelector(".resto-name").innerText = `${restaurantName}`;
-        const ratingIcon = '<i class="fas fa-star"></i>'
-        const ratingIconMissing = '<i class="fas fa-star" id="level-missing"></i>'
-        const rating = (ratingIcon.repeat(marker.details.rating) + ratingIconMissing.repeat(5 - marker.details.rating));
-        document.querySelector(".resto-rating").innerHTML = `${marker.details.food_category}  ${rating}`;
-        document.querySelector(".resto-short-address").innerText = `${marker.details.short_address}`;
-        const sym = "€"
-        const pricing = sym.repeat(marker.details.price_level)
-        document.querySelector(".card-trip-pricing").innerText = pricing;
-        });
-      };
-    });
   };
 };
 
